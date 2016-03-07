a3bcbe65a341c8e16c26b381e9faefa818e7d51a
David Jegat <david.jegat@knplabs.com>
2016-03-07 10:48:31 +0100

    Improve main module

--- lib/main.ts
+++ lib/main.ts
@@ -6,26 +6,7 @@
  *
  * @author David Jegat <david.jegat@gmail.com>
  */
-import SimpleContainer from 'theatre/container/container/simple-container';
-export {SimpleContainer};
-
-import Types from 'theatre/container/definition/types';
-export {Types as DefinitionTypes};
-
-import ContainerError from 'theatre/container/error/container-error';
-export {ContainerError};
-
-import Resolver from 'theatre/container/resolver/resolver';
-export {Resolver};
-
-import ParameterResolver from 'theatre/container/resolver/parameter-resolver';
-export {ParameterResolver};
-
-import ServiceResolver from 'theatre/container/resolver/service-resolver';
-export {ServiceResolver};
-
 import ContainerAggregate from 'theatre/container/container/container-aggregate';
-export {ContainerAggregate};
 
 const container = new ContainerAggregate();
 
--- tests/main-spec.ts
+++ tests/main-spec.ts
@@ -1,18 +1,6 @@
-import * as tct from 'theatre/container';
-import {ContainerAggregate as Container} from 'theatre/container';
 import container from 'theatre/container';
 
-describe('A theatre container', () => {
-    it('contains a container', () => {
-        let a = new Container();
-        let b = new tct.ContainerAggregate();
-
-        expect(a instanceof tct.ContainerAggregate).toBe(true);
-        expect(a instanceof Container).toBe(true);
-        expect(b instanceof tct.ContainerAggregate).toBe(true);
-        expect(b instanceof Container).toBe(true);
-    });
-
+describe('A theatre container main', () => {
     it('contains a default container implementation', () => {
         expect(container.has('foo')).toBe(false);
     });
