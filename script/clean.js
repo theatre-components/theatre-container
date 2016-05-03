a6c8e6a0021c46a98a2f6f37ca44ee1485892833
David Jegat <david.jegat@knplabs.com>
2016-05-03 09:03:11 +0200

    Fix clean script

--- script/clean.js
+++ script/clean.js
@@ -1,8 +1,13 @@
 var fs = require('fs-extra');
+var fs2 = require('fs');
 var args = require('yargs')
     .demand(['path'])
     .argv
 ;
+
+if (!fs2.existsSync(args.path)) {
+    return;
+}
 
 console.log('Remove ' + args.path);
 
